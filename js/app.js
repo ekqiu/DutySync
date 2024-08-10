921050d08100e906ca88513a101610cd3a69dd29
Evan Khee <evankhee@ymail.com>
2024-08-10 21:06:26 +0800

    signout for auth2

--- js/app.js
+++ js/app.js
@@ -71,3 +71,10 @@ function decodeJwtResponse(token) {
 
   return JSON.parse(jsonPayload);
 }
+
+function signOut() {
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log("User signed out.");
+  });
+}
