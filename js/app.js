b215d707ab9930d2f04a7965615ccb6c6582c289
Evan Khee <evankhee@ymail.com>
2024-08-10 18:51:38 +0800

    testing google integration

--- js/app.js
+++ js/app.js
@@ -36,18 +36,34 @@ if ("serviceWorker" in navigator) {
   });
 }
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+function handleCredentialResponse(response) {
+  const responsePayload = decodeJwtResponse(response.credential);
+
+  console.log("ID: " + responsePayload.sub); // Do not send to your backend! Use an ID token instead.
+  console.log("Name: " + responsePayload.name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email); // This is null if the 'email' scope is not present.
   document.getElementById("content").innerHTML = `
     <div>
-      <p>ID: ${profile.getId()}</p>
-      <p>Name: ${profile.getName()}</p>
-      <p>Image URL: ${profile.getImageUrl()}</p>
-      <p>Email: ${profile.getEmail()}</p>
+      <p>ID: ${responsePayload.sub}</p>
+      <p>Name: ${responsePayload.name}</p>
+      <p>Image URL: <img src="${responsePayload.picture}" alt="Profile Image"></p>
+      <p>Email: ${responsePayload.email}</p>
     </div>
   `;
+}
+
+function decodeJwtResponse(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
 }
