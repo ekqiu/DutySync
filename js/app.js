f98255a3e6dc8621ebe8e30735bcb8a07421fa2f
Evan Khee <evankhee@ymail.com>
2024-08-10 18:29:09 +0800

    testing google integration

--- js/app.js
+++ js/app.js
@@ -51,14 +51,3 @@ function onSignIn(googleUser) {
     </div>
   `;
 }
-
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log("User signed out.");
-    // Check if the window is closed
-    if (window.opener) {
-      window.opener.postMessage("signedOut", "*");
-    }
-  });
-}
