8855388941dc7601fef2ad6f679b748119f1e1fe
Evan Khee <evankhee@ymail.com>
2024-08-11 11:07:29 +0800

    update firebase

--- js/app.js
+++ js/app.js
@@ -14,16 +14,18 @@ const allocations = [
 ];
 
 const displayAllocations = () => {
-  let output = `<div><h2>${allocations[0].date}</h2></div>`;
-  allocations[0].deployment.forEach(({ name, member }) => {
-    output += `
-      <div class="card">
-        <h1 class="card--title">${name}</h1>
-        <p class="card--member">${member}</p>
-      </div>
-    `;
-  });
-  content.innerHTML = output;
+  if (allocations[0]) {
+    let output = `<div><h2>${allocations[0].date}</h2></div>`;
+    allocations[0].deployment.forEach(({ name, member }) => {
+      output += `
+        <div class="card">
+          <h1 class="card--title">${name}</h1>
+          <p class="card--member">${member}</p>
+        </div>
+      `;
+    });
+    content.innerHTML = output;
+  }
 };
 
 if (window.location.href.includes("dashboard.html")) {
@@ -100,3 +102,54 @@ function signOut() {
   localStorage.removeItem("responsePayload");
   location.reload();
 }
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBMt0uRtKOH7rLX_kQNbLtZ8gtBUZ9r818",
+    authDomain: "dutifysg.firebaseapp.com",
+    projectId: "dutifysg",
+    storageBucket: "dutifysg.appspot.com",
+    messagingSenderId: "123316970403",
+    appId: "1:123316970403:web:859e1f3f3df7d0ed18f016",
+    measurementId: "G-807BWSW5JC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+// Request permission to send notifications
+function askNotificationPermission() {
+    Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+            console.log("Notification permission granted.");
+            getToken();
+        } else {
+            console.log("Notification permission denied.");
+        }
+    });
+}
+
+// Get FCM token
+function getToken() {
+    messaging.getToken({ vapidKey: "BKeqWzJkekLZMjVhY5rEcqmTLqs5eqfGVgk9e0gybu_vNnPE85aJMGjulDfCqJfu_BNWZTmJHJANVEaUt9Oe1c4" }).then((currentToken) => {
+        if (currentToken) {
+            console.log("FCM Token:", currentToken);
+            // Send the token to your server
+        } else {
+            console.log("No registration token available. Request permission to generate one.");
+        }
+    }).catch((err) => {
+        console.log("An error occurred while retrieving token. ", err);
+    });
+}
+
+// Call the function to ask for notification permission
+askNotificationPermission();
